9c11bb40cf2c3b2ae35a040c742680de213c2890
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-04 10:38:41 -0800

    error

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-SENDER = ARGV[0].scan(/from:\+*\w*/).join[2..-1]
+SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
 RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
 FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
 
