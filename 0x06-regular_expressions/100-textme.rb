7e3cbcdedc4b39f22d41280f04c4e00bd357bceb
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-04 10:29:46 -0800

    100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 SENDER = ARGV[0].scan(/from:\+*\w*/).join[2..-1]
-RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[4..-1]
+RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[5..-1]
 FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
 
 message = "#{SENDER},#{RECEIVER},#{FLAGS}"
