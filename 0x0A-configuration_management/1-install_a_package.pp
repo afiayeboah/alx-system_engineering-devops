57bc5047b54f8768fb27daf9a8dd6240e9952617
afiayeboah <afia.yakuamoah@gmail.com>
2024-02-23 11:01:26 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Using Puppet,install flask from pip3
+# install flask from pip3.
+
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask using pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
