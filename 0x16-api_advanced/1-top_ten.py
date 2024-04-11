a7158884d5959cf7277c981372fdc5180169f209
afiayeboah <afia.yakuamoah@gmail.com>
2024-04-11 08:56:50 -0700

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,10 +13,25 @@ def top_ten(subreddit):
     '''
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Safari/537.36'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        for i in range(10):
-            print(data['data']['children'][i]['data']['title'])
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            for i in range(10):
+                print(data['data']['children'][i]['data']['title'])
+        else:
+            print("Error: Unable to retrieve top ten posts")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: An error occurred during the request - {e}")
+    except Exception as e:
+        print(f"Error: An unexpected error occurred - {e}")
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 filename.py <subreddit>")
     else:
-        print(None)
+        subreddit_name = argv[1]
+        top_ten(subreddit_name)
